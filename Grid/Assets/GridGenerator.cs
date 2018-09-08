5ac40cb70805f27d0cb5105460af3ad32650fdae
Northman94 <Zhan2000@bigmir.net>
2018-09-08 22:48:39 +0300

    Grid and Cells creation
    This commit created RED Grid and separate Cells

---
+++ Grid/Assets/GridGenerator.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GridGenerator : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject tilePrefab;
+
+
+    private int tilesInARow = 10;
+    private int numberOfTiles = 100;
+    private float distanceBetweenTiles = 1.0f; // .1f = beautiful explosion
+
+
+
+
+    private void Start()//
+    {
+        CreateLines();
+
+        CreateCells();
+    }
+
+    #region Grid
+    private float x = -1.5f;
+    private float z = 10.85f;
+    
+    public void CreateLines()
+    {
+        //Vertical
+        //start 0/7/22
+        //end 0/7/13
+        
+        for (int i = 0; i < 11; i++)
+        {
+            x += 1.0f;
+            
+            Debug.DrawLine(new Vector3(x, 0.3f, 11.9f), new Vector3(x, 0.3f, 21.8f), Color.red, Mathf.Infinity); // Math.infinity = время отображения
+        }
+        
+        // Horizontal
+        //x 0-10
+        // z 12-22
+        
+        for (int j = 0; j < 11; j++)
+        {
+            z += 1.0f;
+            Debug.DrawLine(new Vector3(-0.5f, 0.2f, z), new Vector3(9.5f, 0.2f, z), Color.red, Mathf.Infinity);
+        }
+    }
+    #endregion
+
+
+
+    #region Cells
+    public void CreateCells()
+    {
+        float xOffset = 0.0f;
+        float zOffset = 0.0f;
+
+        int tilesCreated;
+
+        for (tilesCreated = 0; tilesCreated < numberOfTiles; tilesCreated++)
+        {
+            xOffset += distanceBetweenTiles;
+
+            if (tilesCreated % tilesInARow == 0)
+            {
+                zOffset += distanceBetweenTiles;
+                xOffset = 0.0f;
+            }
+            //Creating Panels
+            Instantiate(tilePrefab, new Vector3(transform.position.x + xOffset, 0, transform.position.z + zOffset), transform.rotation);
+        }
+    }
+    #endregion
+
+
+
+
+
+
+}
---
+++ Grid/Assets/RayHit.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RayHit : MonoBehaviour 
+{ 
+    //Put this script on camera
+
+    public float rayLength;
+    public LayerMask layerMask;
+
+
+     void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject( ))
+        {
+            RaycastHit hit; // куда записывается инфа в случае попадания
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, rayLength, layerMask))
+            {
+                Debug.Log(hit.collider.name);
+
+
+
+
+
+                //if (hit.collider != null)
+                //{
+                 //   Debug.Log("HIT");
+                //}
+            }
+        }
+
+    }
+}
+
+
+//   string targetX = Convert.ToString(hit.collider.transform.position.x);
+// string targetZ = Convert.ToString(h.collider.transform.position.z);
+
+// Debug.Log(targetX + "  " + targetZ);
